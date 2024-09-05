2e959ea0a1ec5e9426c932b75efaa3b297ae5bad
Seph Gentle <me@josephg.com>
2024-09-06 09:47:00 +1000

    Fixed step0-clean.sh

--- step0-clean.sh
+++ step0-clean.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 rm -f results/*
 rm -rf target/criterion
-cd tools/diamond-types && cargo clean
-cd tools/crdt-converter && cargo clean
-cd tools/ot-bench && cargo clean
-cd tools/paper-benchmarks && cargo clean
+cargo clean --manifest-path tools/diamond-types/Cargo.toml
+cargo clean --manifest-path tools/crdt-converter/Cargo.toml
+cargo clean --manifest-path tools/ot-bench/Cargo.toml
+cargo clean --manifest-path tools/paper-benchmarks/Cargo.toml
